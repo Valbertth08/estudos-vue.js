03847ed94a8f98df62d1bae01cc35e494d3157b1
Valberth.lisboa <valbertth.lisboa@ssp.ma.gov.br>
2025-09-26 09:53:16 -0300

    feat: aplicando a utilização do v-on(passando valores o filho para para o pai)

--- projetos/teste/teste-project/src/App.vue
+++ projetos/teste/teste-project/src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-main>
       <!-- <router-view /> -->
-       <index :nome="pessoa.nome" :idade="pessoa.idade" ></index>
+       <index :nome="pessoa.nome" :idade="pessoa.idade" @nome="exibirNome"></index>
+       <h1>{{ nomeTitle }} </h1>
     </v-main>
   </v-app>
 </template>
@@ -10,14 +11,21 @@
 <script setup>
 import { onMounted, reactive, ref } from 'vue';
 import Index from './pages/index.vue';
+import { no } from 'vuetify/locale';
 
 let pessoa = reactive({
   nome: '',
   idade: 0
 })
 
+let nomeTitle= ref();
+
+const exibirNome=(nome)=>{
+  nomeTitle.value=nome;
+}
+
 onMounted(()=>{
-  pessoa.nome= "pedro"
+  pessoa.nome= "jose"
   pessoa.idade= 12
 })
 </script>
--- projetos/teste/teste-project/src/pages/index.vue
+++ projetos/teste/teste-project/src/pages/index.vue
@@ -6,10 +6,12 @@
   <v-card-text >
     <p> nome: {{ nome }}</p>
     <p> nome: {{ idade }}</p>
+    <v-btn class="btn" @click="$emit('nome',nome)">Exibir nome </v-btn>
   </v-card-text>
   </v-card>
 </template>
 
 <script setup>
  defineProps(["nome","idade"])
+ defineEmits(['nome','idade'])
 </script>
