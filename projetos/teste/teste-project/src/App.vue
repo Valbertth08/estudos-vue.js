545f0cbca6ecf798f601acaddf6adaab5f31b640
Valberth.lisboa <valbertth.lisboa@ssp.ma.gov.br>
2025-09-26 08:54:30 -0500

    feat: aplicando  passagem de valores com v-bind

---
+++ projetos/teste/teste-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <router-view /> -->
+       <index :nome="pessoa.nome" :idade="pessoa.idade" ></index>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import Index from './pages/index.vue';
+
+let pessoa = reactive({
+  nome: '',
+  idade: 0
+})
+
+onMounted(()=>{
+  pessoa.nome= "pedro"
+  pessoa.idade= 12
+})
+</script>
---
+++ projetos/teste/teste-project/src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+  <v-card-title> 
+    Pessoa
+  </v-card-title>
+  <v-card-text >
+    <p> nome: {{ nome }}</p>
+    <p> nome: {{ idade }}</p>
+  </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+ defineProps(["nome","idade"])
+</script>
